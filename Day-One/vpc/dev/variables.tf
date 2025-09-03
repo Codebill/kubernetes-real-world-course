9b09fe7b585e59b39d3e686b2e57a838faa8ffe8
Dj-mac <dj-mac@Dj-macs-MBP.attlocal.net>
2025-09-02 17:37:03 -0700

    Defining a variable that was not declared

--- Day-One/vpc/dev/variables.tf
+++ Day-One/vpc/dev/variables.tf
@@ -2,3 +2,9 @@ variable "environment" {
   type = string
   default = "dev"
 }
+
+variable "k8sVersion" {
+  description = "Kubernetes version for the cluster"
+  type        = string
+  default     = "1.32" # or whatever default makes sense
+}
